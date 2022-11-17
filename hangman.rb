741a517e1d0e62fca3bf14ba5bd5e3488f6c5820
kdoni <chrisvdoni@gmail.com>
2022-11-17 17:59:42 +0200

    On the right track.

--- hangman.rb
+++ hangman.rb
@@ -12,50 +12,35 @@ end
 class Hangman
   @@game_over = false
   @@guess = ''
-  @@target = accepted.sample
+  @@target = accepted.sample.split('')
   @@attempts = 10
   @@unknown = Array.new(@@target.length, '-')
   @@alphabet = 'abcdefghijklmnopqrstuvwxyz'
   @@discarded_letter_array = []
+  @@accepted_letters_array = []
 
   def self.start_game
     puts "\nWelcome to a game of Hangman! Ready for a challenge?\n\n"
     puts "Try finding the hidden word.\n\n"
     puts @@unknown.join('')
     puts "You've got #{@@attempts} attempts to do so."
+    p @@target
     play_game
   end
 
-  def self.check_guess
-    while @@game_over == false && @@attempts > 0
-      if @@guess.length != 1 || !@@alphabet.split('').include?(@@guess.downcase)
-        puts "invalid input, try another letter\n"
-        play_game
-      else
-        @@attempts -= 1
-        game_progress
-      end
-    end
-  end
-
-  def self.discard_letter
-    if @@alphabet.include?(@@guess.downcase) == false && @@discarded_letter_array.include?(@@guess.downcase) == false
-      @@discarded_letter_array.push(@@guess)
-    end
-  end
-
-  def self.game_progress
-    if @@target.split('').include?(@@guess)
-      puts 'true'
-    else
-      puts 'false'
-    end
-  end
-
   def self.play_game
-    puts 'Choose a letter.'
-    @@guess = gets.chomp
-    check_guess
+    while @@game_over == false
+      @@guess = gets.chomp
+      check_guess
+    end
+  end
+
+  def self.check_guess
+    if @@target.include?(@@guess)
+      puts 'good'
+    else
+      puts 'bad'
+    end
   end
 end
 Hangman.start_game
