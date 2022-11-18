1a977cdbcc7588479e71d8e1756cc8b6806bb043
kdoni <chrisvdoni@gmail.com>
2022-11-18 13:44:50 +0200

    Last strido.

--- hangman.rb
+++ hangman.rb
@@ -22,7 +22,6 @@ class Hangman
   def self.start_game
     puts "\nWelcome to a game of Hangman! Ready for a challenge?\n\n"
     puts "Try finding the hidden word.\n\n"
-    puts @@unknown.join('')
     puts "You've got #{@@attempts} attempts to do so."
     p @@target
     play_game
@@ -30,30 +29,56 @@ class Hangman
 
   def self.play_game
     while @@game_over == false
+      puts @@unknown.join('')
       @@guess = gets.chomp
+      winning_condition
+      check_invalid_guesses
+      repeated_word
       check_guess
-      game_is_over
+    end
+  end
+
+  def self.check_invalid_guesses
+    while @@alphabet.include?(@@guess.downcase) == false
+      puts 'invalid input, try again...'
+      play_game
+    end
+  end
+
+  def self.repeated_word
+    while @@discarded_letter_array.include?(@@guess.downcase) || @@accepted_letters_array.include?(@@guess.downcase) && @@game_over == false
+      puts 'repetion...'
+      play_game
+    end
+  end
+
+  def self.winning_condition
+    if @@unknown.include?('-') == false
+      @@game_over = true
+      puts "Congratulations, you've found the word #{@@unknown.join('').capitalize}, you've won!"
     end
   end
 
   def self.game_is_over
     if @@attempts == 0
       puts "You've lost. Game Over..."
-      @game_over = true
+      @@game_over = true
     end
   end
 
   def self.check_guess
-    if @@target.include?(@@guess)
+    if @@target.include?(@@guess.downcase)
       @@accepted_letters_array.push(@@guess)
       @@target.each_with_index do |v, i|
-        @@unknown[i] = v if v == @@guess
+        @@unknown[i] = v if v == @@guess.downcase
       end
     else
+      puts 'Incorrect word...'
       @@discarded_letter_array.push(@@guess)
       @@attempts -= 1
+      puts "you've got #{@@attempts} attempts left... : Your discarded letters' pile: #{@@discarded_letter_array}"
+      game_is_over
     end
-    p @@unknown
   end
 end
 Hangman.start_game
